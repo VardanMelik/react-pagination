0c4f19ee0dc36b4565964a66af0c64f0027c5645
EYE MOTION <eyemotiongamestudio@gmail.com>
2019-12-14 18:50:10 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import Posts from './components/Posts'
 import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [postsPerPage, setPostsPerPage] = useState(10)
+
+    useEffect( () => {
+      const fetchPosts = async () => {
+        setLoading(true)
+        const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setPosts(res.data)
+        setLoading(false)
+      }
+      fetchPosts()
+    }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container mt-5">
+        <h1 className="text-primary mb-3">My Blog</h1>
+        <Posts posts={posts} loading={loading} /> 
+      </div>
     </div>
   );
 }
